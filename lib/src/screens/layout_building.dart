7fe20c094698b50fc9c4f18abaf4efbd1bcd2281
Vijay <vijayganganapalli@gmail.com>
2018-09-11 18:43:13 +0530

    description section added and aply list view

--- lib/src/screens/layout_building.dart
+++ lib/src/screens/layout_building.dart
@@ -8,17 +8,22 @@ class LayoutBuilding extends StatefulWidget {
 class _LayoutBuildingState extends State<LayoutBuilding> {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: <Widget>[
         imageSection(),
         titleSection(),
         buttonSection(),
+        descriptionSection(),
       ],
     );
   }
 
   Widget imageSection() {
-    return Image.asset("images/lake.jpg");
+    return Image.asset(
+      "images/lake.jpg",
+      height: 240.0,
+      fit: BoxFit.cover,
+    );
   }
 
   Widget titleSection() {
@@ -86,6 +91,18 @@ class _LayoutBuildingState extends State<LayoutBuilding> {
           ),
         ),
       ],
+    );
+  }
+
+  Widget descriptionSection() {
+    String description =
+        "Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.";
+    return Container(
+      padding: const EdgeInsets.all(32.0),
+      child: Text(
+        description,
+        softWrap: true,
+      ),
     );
   }
 }
