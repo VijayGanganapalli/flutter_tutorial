2015ab9d39731bcbb2d63bdb3129f3d4303dad9c
Vijay <vijayganganapalli@gmail.com>
2018-09-11 17:29:21 +0530

    title section added

--- lib/src/screens/layout_building.dart
+++ lib/src/screens/layout_building.dart
@@ -10,8 +10,48 @@ class _LayoutBuildingState extends State<LayoutBuilding> {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Image.asset("images/lake.jpg"),
+        imageSection(),
+        titleSection(),
       ],
+    );
+  }
+
+  Widget imageSection() {
+    return Image.asset("images/lake.jpg");
+  }
+
+  Widget titleSection() {
+    return Container(
+      padding: const EdgeInsets.all(32.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    "Oeschinen Lake Campground",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  "Kandersteg, Switzerland",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          Text("41"),
+        ],
+      ),
     );
   }
 }
